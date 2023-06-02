b48ed3be6b13a19159a45d1d291cc08556c6ad54
Alexandre DRUT <alexandre.drut@gmail.com>
2023-06-02 13:20:45 +0200

    modification into update file

--- Update_v7.ps1
+++ Update_v7.ps1
@@ -8,16 +8,36 @@ function Update
 	Write-Host [(Get-Date).ToLongTimeString()] "Number of update found : " $nbUpdateToDo
 	if ($nbUpdateToDo -gt 0)
 	{ 
-		Write-Host [(Get-Date).ToLongTimeString()] "Update is running"
 		foreach ($items in $update) 
 		{
 			try 
 			{
 				Write-Host [(Get-Date).ToLongTimeString()] "Start installation" 
 				Write-Host [(Get-Date).ToLongTimeString()] ($items | Select-Object -Property Title)
-				Install-WindowsUpdate -AcceptAll -install
-				Write-Host [(Get-Date).ToLongTimeString()] ("Installation finished : " + ($items | Select-Object -Property KB))    
-				Write-Host [(Get-Date).ToLongTimeString()] (($nbUpdateToDo -eq 1) ? "Update is finished" : "Update are finished")
+				$install = Install-WindowsUpdate -AcceptAll -install -KB $items.KB
+				if($install[2].result -eq "Installed")
+				{
+				Write-Host [(Get-Date).ToLongTimeString()] ("Installation finished : " + ($items | Select-Object -Property KB)) 
+				
+				$status = Get-WURebootStatus
+				if ($status.RebootRequired -eq 1) 
+				{
+					Write-Error "Restart in progress"
+					shutdown.exe /r /t 60 
+					return
+				}
+				else
+				{ 
+					Write-Host [(Get-Date).ToLongTimeString()] " Not necessary to restart computer"; 
+				}   
+
+				
+				
+				}
+				else
+				{
+					Write-Host [(Get-Date).ToLongTimeString()] ("Installation failed : " + ($items | Select-Object -Property KB))    
+				}
 			}
 			catch 
 			{
@@ -25,17 +45,9 @@ function Update
 			}
 			Write-Host [(Get-Date).ToLongTimeString()] "End time"
 		}
-    
-		$status = Get-WURebootStatus
-			if ($status.RebootRequired -eq 1) 
-			{
-				Write-Error "Restart in progress"
-				shutdown.exe /r /t 60 
-			}
-			else
-			{ 
-				Write-Host [(Get-Date).ToLongTimeString()] "Not necessary to restart computer"; 
-			}   
+		
+		write-host [(Get-Date).ToLongTimeString()] (($nbUpdateToDo -eq 1) ? "Update is finished" : "Update are finished")
+		
 	}
 	else 
 	{ 
